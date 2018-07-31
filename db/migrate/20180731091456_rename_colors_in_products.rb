444fceca537c70e296abf0233622c2f4c73df64a
adam pauw <adampauw@gmail.com>
2018-07-31 11:58:38 +0200

    4.2 Add migration number [5.5]

--- db/migrate/20180731091456_rename_colors_in_products.rb
+++ db/migrate/20180731091456_rename_colors_in_products.rb
@@ -1,4 +1,4 @@
-class RenameColorsInProducts < ActiveRecord::Migration
+class RenameColorsInProducts < ActiveRecord::Migration[5.2]
   def change
     rename_column :products, :color, :colour
   end
