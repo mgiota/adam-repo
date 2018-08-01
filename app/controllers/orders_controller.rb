102d426161b64d4bc595cd0a5e3f9f452069614d
adam pauw <adampauw@gmail.com>
2018-08-02 00:02:57 +0200

    4.4 Add orders controller/routes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,19 @@
 class OrdersController < ApplicationController
 end
+
+class OrdersController < ApplicationController
+    def index
+    end
+  
+    def show
+    end
+  
+    def new
+    end
+  
+    def create
+    end
+  
+    def destroy
+    end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :products
+  resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
