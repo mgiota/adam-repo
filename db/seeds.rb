0a8a15b447b06591086b50472c29f9a8d59cf321
adam pauw <adampauw@gmail.com>
2018-08-01 23:51:15 +0200

    4.3 Create landing page/orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: https://cdn.pixabay.com/photo/2017/11/04/11/37/old-2917289_960_720.jpg)
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "https://cdn.pixabay.com/photo/2017/11/04/11/37/old-2917289_960_720.jpg")
