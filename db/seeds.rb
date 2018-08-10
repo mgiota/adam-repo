268629e548abe1fd2125f808f84313db0c2a1150
adam pauw <adampauw@gmail.com>
2018-08-10 23:44:17 +0200

    4.6 Add some css / update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "https://cdn.pixabay.com/photo/2017/11/04/11/37/old-2917289_960_720.jpg")
+Product.create(name: "seed-generated product", description: "I also created this product without using the HTML form!", image_url: "https://cdn.pixabay.com/photo/2017/11/04/11/37/old-2917289_960_720.jpg", price: 505)
 
-Order.create(user_id: 1, product_id: 1, total: 10.0)
-Order.create(user_id: 1, product_id: 2, total: 5.0)
+Order.create(user_id: 1, product_id: 1, price: 500, total: 10.0)
+Order.create(user_id: 1, product_id: 2, price: 1000, total: 500.0)
