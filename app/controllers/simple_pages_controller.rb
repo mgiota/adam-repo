302bfc9b3ecf7fe6b6f2ed2b518bf8f41559b26b
adam pauw <adampauw@gmail.com>
2018-09-05 15:38:13 -0700

    5.3 Add mailers

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,6 +1,17 @@
 class SimplePagesController < ApplicationController
   def index
   end
+
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(from: @email,
+        to: 'adampauw@gmail.com',
+        subject: "A new contact form message from #{@name}",
+        body: @message).deliver_now
+  end
+
 end
 
 class SimplePagesController < ApplicationController
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'brokeback@example.com'
   layout 'mailer'
 end
