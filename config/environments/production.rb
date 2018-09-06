763e465762680191e98df4359b3c9776f2160944
adam pauw <adampauw@gmail.com>
2018-09-05 18:27:07 -0700

    5.4 Add Devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #Ensure dynamic links shown in emails generate the correct URLs
+  config.action_mailer.default_url_options = { host: 'broke-bike-mountain.herokuapp.com' }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :users
   resources :products
   resources :orders, only: [:index, :show, :create, :destroy]
