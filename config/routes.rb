0f5e4283be507bd8a4cb3ae5b035825d12e3a5f4
adam pauw <adampauw@gmail.com>
2018-09-17 22:07:41 +0200

    5.7 comment issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: {sign_in: "login", sign_out: "logout"}, controllers: { registrations: "user_registrations" }
+  resources :products do
+    resources :comments
+  end
   resources :users
   resources :products
+  resources :categories
   resources :orders, only: [:index, :show, :create, :destroy]
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  root 'simple_pages#landing_page'
   post 'simple_pages/thank_you'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'simple_pages#landing_page'
+  post 'payments/create'
+  mount ActionCable.server => '/cable'
+
 end
