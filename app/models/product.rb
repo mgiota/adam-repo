cf62f0ecc91a534ef60bcc30b08eb466cbd0d6b4
adam pauw <adampauw@gmail.com>
2018-09-20 11:28:38 +0200

    5.7 show style and validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
     validates :name, presence: true
-    belongs_to :category
     has_many :orders
     has_many :comments
+    
     def self.search(search_term)
         if Rails.env.development?
           # Dev Mode (sqlite3)
@@ -12,9 +12,15 @@ class Product < ApplicationRecord
           Product.where("name ilike ?", "%#{search_term}%")
         end
       end
+
       def highest_rating_comment
         comments.rating_desc.first
       end
+
+      def lowest_rating_comment
+        comments.rating_asc.first
+      end
+
       def average_rating
         comments.average(:rating).to_f
       end
