6e573acf8c1d2936d8be37cd91f47c72b0ec794e
adam pauw <adampauw@gmail.com>
2018-09-21 12:18:09 +0200

    5.9 Comment controller end

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,20 @@
 class CommentsController < ApplicationController
     def create
-        @product = Product.find(params[:product_id])
-        @comment = @product.comments.new(comment_params)
-        @comment.user = current_user
-        respond_to do |format|
-          if @comment.save
-            #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-            #ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
-            format.html { redirect_to @product, notice: 'Review was created successfully.' }
-            format.json { render :show, status: :created, location: @product }
-            format.js
-          else
-            format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
-            format.json { render json: @comment.errors, status: :unprocessable_entity }
-          end
-    
+      @product = Product.find(params[:product_id])
+      @comment = @product.comments.new(comment_params)
+      @comment.user = current_user
+      respond_to do |format|
+        if @comment.save
+          #ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+          #ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
+          format.html { redirect_to @product, notice: 'Review was created successfully.' }
+          format.json { render :show, status: :created, location: @product }
+          format.js
+        else
+          format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
+          format.json { render json: @comment.errors, status: :unprocessable_entity }
+        end
+      end
     end
   
     def destroy
@@ -24,5 +24,4 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:user_id, :body, :rating)
     end
-  end
 end
