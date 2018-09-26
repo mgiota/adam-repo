89a9bf99e6c3c5e5b770c05b516b408ed39344f6
adam pauw <adampauw@gmail.com>
2018-09-26 10:14:10 +0200

    5.10 User registration fix

--- app/controllers/user_registrations_controller.rb
+++ app/controllers/user_registrations_controller.rb
@@ -2,7 +2,6 @@ class UserRegistrationsController < Devise::RegistrationsController
   def create
     super
     if @user.persisted?
-      UserMailer.welcome(@user).deliver_now
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,4 @@ Rails.application.routes.draw do
   get 'simple_pages/contact'
   get 'simple_pages/index'
   root 'simple_pages#landing_page'
-  post 'simple_pages/thank_you'
 end
