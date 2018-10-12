d915e48b1bbe4c9a3e2aaeabbdd26b6032451315
adam pauw <adampauw@gmail.com>
2018-10-12 21:43:09 +0200

    Styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get 'simple_pages/about'
   get 'simple_pages/contact'
   get 'simple_pages/index'
-  post 'simple_pages/thank_you'
+  # post 'simple_pages/thank_you'
   root 'simple_pages#landing_page'
-  post 'payments/create'
+  # post 'payments/create'
 end
