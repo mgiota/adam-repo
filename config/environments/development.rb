c7dab271ffc9f0d308513c8205c16912a5a74308
adam pauw <adampauw@gmail.com>
2018-10-20 09:45:42 +0200

    6.11 Caching fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,9 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  # Show fragment caching on terminal
+  config.action_controller.enable_fragment_cache_logging = true
+
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
