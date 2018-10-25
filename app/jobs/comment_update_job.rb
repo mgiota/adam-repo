6efa080ae6776aab56fb34167c54868d1caa3b27
adam pauw <adampauw@gmail.com>
2018-10-25 10:58:04 +0200

    6.14 Fixes

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -3,7 +3,7 @@ class CommentUpdateJob < ApplicationJob
 
   def perform(comment, current_user)
     # Do something later
-    ProductChannel.broadcast_to (comment.product_id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: comment, current_user: current_user}), average_rating: @product.average_rating
+    ProductChannel.broadcast_to(comment.product_id, comment: render_comment(comment, current_user), average_rating: comment.product.average_rating)
   end
 
   private
